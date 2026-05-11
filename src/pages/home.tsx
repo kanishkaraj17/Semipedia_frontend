9cc734e05f5de29dab5736de0acf098f0688f55d
kanishkaraj17 <65603686+kanishkaraj17@users.noreply.github.com>
2026-05-11 21:04:42 +0530

    Update home.tsx

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -4,7 +4,8 @@ import { Button } from "../components/ui/button";
 import { Input } from "../components/ui/input";
 import { Card, CardContent } from "../components/ui/card";
 import { ScrollArea } from "../components/ui/scroll-area";
-import { MarkdownRenderer } from "../components/markdown-renderer";
+import { MarkdownRenderer } from "@/components/MarkdownRenderer";
+
 import { cn } from "../lib/utils";
 
 type HistoryItem = {
